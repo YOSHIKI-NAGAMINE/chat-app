f22e07d47926625206eaf63ffb9100d6f3528f55
YOSHIKI-NAGAMINE <y.nagamine1236@gmail.com>
2023-02-15 14:38:51 +0900

    チャットルームの削除機能の実装

--- app/models/room.rb
+++ app/models/room.rb
@@ -2,9 +2,9 @@ class Room < ApplicationRecord
   def index
   end
 
-  has_many :room_users
+  has_many :room_users, dependent: :destroy
   has_many :users, through: :room_users
-  has_many :messages
+  has_many :messages, dependent: :destroy
 
   validates :name, presence: true
 end
